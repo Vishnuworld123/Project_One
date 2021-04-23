f8148542b83bc5d651c8f85e457edcce7ff90b45
Chetan Mahajan <mr.chetanbmahajan@gmail.com>
2021-04-23 23:10:28 +0530

    Added a serializer

--- bookinfo/serializers.py
+++ bookinfo/serializers.py
@@ -1,8 +1,13 @@
-from bookinfo.models import Book
+from bookinfo.models import Book, Dreamreal
 from rest_framework import serializers
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
+        exclude = ['is_deleted']
+
+class DreamrealSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Dreamreal
         exclude = ['is_deleted']
